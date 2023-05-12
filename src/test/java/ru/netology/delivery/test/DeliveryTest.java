cdf4160b776764599f2eac8504c50ead1e5d21d8
Elena Nekrasova <kvazavr@yandex.ru>
2023-05-12 10:25:04 +0400

    Modified tests

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -28,7 +28,6 @@ class DeliveryTest {
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-//        open("http://localhost:9999/");
         $("[type = text]").setValue(generateCity());
         $("[data-test-id=date]").click();
         $("[data-test-id=date] [value]").sendKeys(Keys.CONTROL + "a");
@@ -38,19 +37,15 @@ class DeliveryTest {
         $("[name = phone]").setValue(DataGenerator.generatePhone());
         $("[data-test-id=agreement]").click();
         $(".button__text").click();
-        $(".notification__content").shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate), Duration.ofSeconds(2)).shouldBe(Condition.visible);
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate)).shouldBe(Condition.visible);
         $("[data-test-id=date]").click();
         $("[data-test-id=date] [value]").sendKeys(Keys.CONTROL + "a");
         $("[data-test-id=date] [value]").sendKeys(Keys.BACK_SPACE);
         $("[data-test-id=date] [value]").setValue(secondMeetingDate);
         $(".button__text").click();
-        $(".notification__content").shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?")).shouldBe(Condition.visible);
-        $x("Перепланировать").click();
-        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + secondMeetingDate)).shouldBe(Condition.visible);
-        // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
-        // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
-        // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
-        // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
-        // имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
+        $x("//*[contains(text(), 'Необходимо подтверждение')]").shouldBe(Condition.visible);
+        $x("//*[contains(text(), 'Перепланировать')]").click();
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно запланирована на " + secondMeetingDate)).shouldBe(Condition.visible);
+
     }
 }
